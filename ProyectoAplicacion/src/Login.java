ff04b840f7027fdade1ab4e948ef54e5b0cb72a4
AlexSecundino <alex.secundino@gmail.com>
2014-04-03 20:55:03 +0200

    Insertar conexión con la base de datos para realiar el login.

--- ProyectoAplicacion/src/Login.java
+++ ProyectoAplicacion/src/Login.java
@@ -2,19 +2,26 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 import java.awt.Font;
 
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.*;
 
 public class Login extends JFrame {
 
-	private JPanel contentPane;
+	private static JPanel contentPane;
 	private JTextField txtUsuario;
 	private JTextField txtPassword;
-
+	private static Connection conection;
 	/**
 	 * Launch the application.
 	 */
@@ -24,6 +31,21 @@ public class Login extends JFrame {
 				try {
 					Login frame = new Login();
 					frame.setVisible(true);
+					
+					try{
+						Class.forName("com.mysql.jdbc.Driver");
+						conection = DriverManager.getConnection("jdbc:mysql://localhost/prueba", "root", "");
+						JOptionPane.showMessageDialog(contentPane, "Conexión realizada con éxito.");
+					}
+					catch(ClassNotFoundException e){
+						JOptionPane.showMessageDialog(contentPane, e.getMessage());
+					}
+					catch(SQLException e){
+						JOptionPane.showMessageDialog(contentPane, e.getMessage());
+					}
+					catch(Exception e){
+						JOptionPane.showMessageDialog(contentPane, e.getMessage());
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,6 +57,7 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -42,30 +65,51 @@ public class Login extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblLogin = new JLabel("Login");
-		lblLogin.setBounds(170, 11, 46, 14);
-		contentPane.add(lblLogin);
-		
 		JLabel lblUsuario = new JLabel("Usuario:");
 		lblUsuario.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		lblUsuario.setBounds(50, 81, 100, 50);
+		lblUsuario.setBounds(50, 49, 100, 50);
 		contentPane.add(lblUsuario);
 		
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		lblPassword.setBounds(50, 128, 100, 50);
+		lblPassword.setBounds(50, 110, 100, 50);
 		contentPane.add(lblPassword);
 		
 		txtUsuario = new JTextField();
 		txtUsuario.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		txtUsuario.setBounds(150, 78, 150, 50);
+		txtUsuario.setBounds(150, 50, 150, 50);
 		contentPane.add(txtUsuario);
 		txtUsuario.setColumns(10);
 		
 		txtPassword = new JTextField();
 		txtPassword.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		txtPassword.setBounds(150, 135, 150, 50);
+		txtPassword.setBounds(150, 110, 150, 50);
 		contentPane.add(txtPassword);
 		txtPassword.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(contentPane, "Conectando...");
+			
+				String consulta2 = "select * from clientes where Usuario='"
+						+ txtUsuario.getText() + "' AND Password='" + txtPassword.getText() + "'";
+				
+				try {
+					Statement consulta = conection.createStatement();
+					ResultSet resultado = consulta.executeQuery(consulta2);
+					
+					if(resultado.next())
+						JOptionPane.showMessageDialog(contentPane, "¡Correcto!");
+					else
+						JOptionPane.showMessageDialog(contentPane, "Datos incorrectos");
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(contentPane, e.getMessage());
+				}
+				
+			}
+		});
+		btnLogin.setBounds(180, 212, 89, 23);
+		contentPane.add(btnLogin);
 	}
 }
